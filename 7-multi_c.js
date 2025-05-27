e0b9b70992df0f481e33727c167446983461b46b
Nwekeson Chibuzor <zaddychiboy@gmail.com>
2025-05-27 11:57:21 +0100

    Print C is fun x times or error message if invalid

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,10 +1,11 @@
 const arg = process.argv[2];
 const count = parseInt(arg);
 
-if (isNaN(count) || count <= 0) {
+if (!arg || isNaN(count)) {
   console.log("Missing number of occurrences");
-} else {
+} else if (count > 0) {
   for (let i = 0; i < count; i++) {
     console.log("C is fun");
   }
 }
+// If count <= 0, do nothing (no output)
